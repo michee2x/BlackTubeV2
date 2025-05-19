1e94c65d2e17e3df47969a3f8d182354861101ea
michee2x <michaelisraelmike@gmail.com>
2025-05-20 00:06:09 +0100

    profile update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { BiSolidBookmark, BiSolidLike } from "react-icons/bi";
 import { BsIncognito } from "react-icons/bs";
 import { CiMenuKebab } from "react-icons/ci";
 import { FiPlus } from "react-icons/fi";
 import { IoMdShareAlt } from "react-icons/io";
-import { IoLogoGoogle } from "react-icons/io5";
-import { MdPerson } from "react-icons/md";
+import { IoLogoGoogle, IoTimeOutline } from "react-icons/io5";
+import { MdMovie, MdOutlineInsertChart, MdPerson } from "react-icons/md";
+import { LiaDownloadSolid } from "react-icons/lia";
+import { FaGraduationCap, FaRegQuestionCircle, FaVideo } from "react-icons/fa";
+import { GoVideo } from "react-icons/go";
 
 type data = {
   title: string;
@@ -45,7 +49,7 @@ const page = () => {
   ];
 
   return (
-    <main className="pl-2 text-white bg-black min-h-screen">
+    <main className=" text-white bg-black min-h-screen">
       <div className="flex flex-col justify-center items-center gap-4 mb-6">
         <img
           className="w-24 rounded-full h-24 object-cover"
@@ -74,7 +78,7 @@ const page = () => {
         </nav>
       </div>
       {/* History */}
-      <section>
+      <section className="pl-2">
         <h2 className="text-lg w-full h-auto flex justify-between items-center font-semibold mb-4">
           <span>History</span>
           <button className="px-4 flex items-center gap-1 text-nowrap w-auto h-8 text-[10px] cursor-pointer py-1  border-[.3px] border-gray-600 rounded-full bg-black hover:bg-[#424242a1]">
@@ -106,7 +110,7 @@ const page = () => {
       </section>
 
       {/* Playlists */}
-      <section>
+      <section className="pl-2">
         <h2 className="text-lg w-full mt-8 h-auto flex justify-between items-center font-semibold mb-4">
           <span>Playlists</span>
           <span className="flex items-center gap-2 ">
@@ -119,11 +123,26 @@ const page = () => {
         <div className="flex min-w-full hide-scrollbar lg:overflow-x-auto h-auto space-x-2 py-2 bg-black">
           {historyItems.map((item, i) => (
             <div key={i} className="relative min-w-[45vw] h-40">
-              <img
-                src={item.thumbnail}
-                alt={item.title}
-                className="rounded-lg w-full h-24 object-cover"
-              />
+              <div>
+                <div className="w-full h-[6.4rem] relative">
+                  <div className="h-full w-[95%] rounded-xl -translate-x-1/2 left-1/2 bg-gray-400 absolute bottom-0"></div>
+                  <img
+                    src={item.thumbnail}
+                    alt={item.title}
+                    className="rounded-lg bottom-0 w-full z-10 absolute h-24 object-cover"
+                  />
+                  <div className="w-full text-[14px] bottom-0 flex flex-col gap-1.5 items-center justify-center h-24 z-20 bg-black/50 absolute">
+                    {i === 0 ? (
+                      <BiSolidLike className="text-xl text-white" />
+                    ) : i === 1 ? (
+                      <IoTimeOutline className="text-xl text-white" />
+                    ) : (
+                      <BiSolidBookmark className="text-xl text-white" />
+                    )}
+                    {i === 0 ? 152 : i === 1 ? 68 : 5}
+                  </div>
+                </div>
+              </div>
               <div className="mt-2">
                 <h3 className="text-sm flex gap-1 justify-between font-semibold line-clamp-2">
                   {`${item.title.slice(0, 30)}...`}{" "}
@@ -141,26 +160,48 @@ const page = () => {
       </section>
 
       {/* Watch later, liked videos, your clips */}
-      <section className="mt-10 space-y-6 text-sm">
-        <div>
-          <p className="font-medium">Watch later</p>
-          <p className="text-gray-400">
-            Save videos to watch later. Your list shows up right here.
-          </p>
-        </div>
-        <div>
-          <p className="font-medium">Liked videos</p>
-          <p className="text-gray-400">
-            Use the thumbs up icon to like videos. Your list shows up right
-            here.
-          </p>
-        </div>
-        <div>
-          <p className="font-medium">Your clips</p>
-          <p className="text-gray-400">
-            Clip and share your favorite moments. Your list shows up right here.
-          </p>
-        </div>
+      <section className="mt-10 flex border-b-[2px] pb-3 border-gray-600 flex-col space-y-6 text-sm">
+        {[
+          { icon: <GoVideo />, name: "Your Video" },
+          { icon: <LiaDownloadSolid />, name: "Downloads" },
+          { icon: <FaGraduationCap />, name: "Your Courses" },
+          { icon: <IoMdShareAlt />, name: "Share channel" },
+        ].map((i, idx) => {
+          return (
+            <div key={idx} className="flex gap-6 px-6 items-center py-2">
+              <span className="text-2xl text-white">{i.icon}</span>
+              <p className="text-gray-400">{i.name}</p>
+            </div>
+          );
+        })}
+      </section>
+
+      <section className="mt-5 flex border-b-[2px] pb-3 border-gray-600 flex-col space-y-6 text-sm">
+        {[
+          { icon: <MdMovie />, name: "Your Movies" },
+          { icon: <FaVideo />, name: "Get Youtube Premium" },
+        ].map((i, idx) => {
+          return (
+            <div key={idx} className="flex gap-6 px-6 items-center py-2">
+              <span className="text-2xl text-white">{i.icon}</span>
+              <p className="text-gray-400">{i.name}</p>
+            </div>
+          );
+        })}
+      </section>
+
+      <section className="mt-5 flex border-b-[2px] pb-3 border-gray-600 flex-col space-y-6 text-sm">
+        {[
+          { icon: <MdOutlineInsertChart />, name: "Time Watched" },
+          { icon: <FaRegQuestionCircle />, name: "Help & feedback" },
+        ].map((i, idx) => {
+          return (
+            <div key={idx} className="flex gap-6 px-6 items-center py-2">
+              <span className="text-2xl text-white">{i.icon}</span>
+              <p className="text-gray-400">{i.name}</p>
+            </div>
+          );
+        })}
       </section>
     </main>
   );
